6455be68714ac352a11df98582deaa159dc180b6
ramakrishna <pvrk143@gmail.com>
2019-05-06 21:44:14 +0530

    toronto project in python pushing to github

---
+++ python_code.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import numpy as np
+import requests
+from bs4 import BeautifulSoup
+
+source = requests.get('https://en.wikipedia.org/wiki/List_of_postal_codes_of_Canada:_M').text
+
+soup=BeautifulSoup(source, 'html5lib')
+
+postal_codes_dict = {}for table_cell in soup.find_all('td'):
+    try:
+        postal_code=table_cell.p.b.text
+        postal_code_investigate=table_cell.span.text
+        neighborhoods_data=table_cell.span.text
+        borough=neighborhoods_data.split('(')[0]
+        if neighborhoods_data == 'Not assigned':
+            neighborhoods = [] else:
+                postal_codes_dict[postal_code]={} try:
+                    neighborhoods=neighborhoods_data.split('(')[1]
+                    neighborhoods=neighborhoods.replace('(','')
+                    neighborhoods=neighborhoods.replace(')','')
+                    neighborhoods_names=neighborhoods.split('/')
+                    neighborhoods_clean=','.join([name.strip()for name in neighborhoods_clean = borough
+                        postal_codes_dict[postal_code]['neighborhoods']= neighborhoods_clean
+                        except:
+                        pass
+columns= ['PostalCode', 'Borough', 'Neighborhood']
+toronto_data = pd.DataFrame(columns=columns)
+toronto_data
+
+for ind,postal_code in enumerate
+(postal_codes_dict):
+borough = postal_codes_dict[postal_code]['borough']
+neighborhood = postal_codes_dict[postal-code]['neighborhoods']
+toronto_data = toronto_data.append({"PostalCode":postal_code, "Borough":borough,
+    "Neighborhood":neighborhood},
+    ignore_index=True)
+toronto data.shape[0]
+
+
+
+
+
+
+
+
+
+
